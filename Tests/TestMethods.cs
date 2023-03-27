b6ac802adeacc57bccc5799644253ca43c9bd76d
Михаил Зюляев <zyulyaev12@gmail.com>
2023-03-27 22:39:50 +0400

    Final

--- Tests/TestMethods.cs
+++ Tests/TestMethods.cs
@@ -249,48 +249,15 @@ namespace TestsMethods
             var airbase = new Airbase("Нытва");
             var plane = TestData.listPlanes[new Random().Next(TestData.listPlanes.Count)];
             var checkListNameSensors = TestData.listNameSensors;
+            var expectedReport = TestData.CreateReport(pilot, plane, checkListNameSensors);
+
             commander.SetFlight(airbase, pilot, plane, checkListNameSensors);
             pilot.StartFlight();
             pilot.CheckSensors();
             pilot.FinishFlight();
 
-            var report = $"Пилот: {pilot.Name}\n";
-            if (plane.AirborneSystem.Sensors.Any(p => p.HaveSoftwareProblem == true && p.IsFixedSoftwareProblem == false))
-            {
-                report += $"Самолёт: ID {plane.Id}, Model {plane.Model}, Exist False\n";
-            }
-            else
-                report += $"Самолёт: ID {plane.Id}, Model {plane.Model}, Exist True\n";
-            report += "Датчики в самолёте:\n";
-            foreach (var sensor in plane.AirborneSystem.Sensors)
-                report += $"\t{sensor.Name}\n";
-            if (checkListNameSensors.Count > 0)
-            {
-                report += "Результат проверки:\n";
-                report += "\tДатчики для проверки:\n";
-                foreach (var sensorName in checkListNameSensors)
-                    report += $"\t\t{sensorName}\n";
-
-                if (plane.AirborneSystem.Sensors.Any(p => p.HaveSoftwareProblem == true))
-                {
-                    report += "\tСломанные датчики\n";
-                    var sensors = TestData.listSensors.Where(p => p.HaveSoftwareProblem == true);
-                    foreach (var sensor in sensors)
-                    {
-                        if (sensor.IsFixedSoftwareProblem == true)
-                            report += $"\t\t{sensor.Name}, Исправлен True\n";
-                        else
-                            report += $"\t\t{sensor.Name}, Исправлен False\n";
-
-                        if (sensor.IsFixedSoftwareProblem == false)
-                            break;
-                    }
-                        
-                }
-            }
-
             var resultReport = pilot.CreateReport(commander);
-            Assert.That(resultReport, Is.EqualTo(report));
+            Assert.That(resultReport, Is.EqualTo(expectedReport));
         }
     }
 
